444fb1fd82148d305d37151db1e43ca726a4d306
Letícia Lima <leticialimaferreira2@gmail.com>
2021-05-02 10:00:25 -0300

    calculadora de medias e aprovacao

---
+++ src/com/company/CalculaMedia.java
@@ -0,0 +1,37 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class CalculaMedia {
+    public void media(){
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Informe a nota 1: ");
+        Double nota1 = input.nextDouble();
+
+        System.out.println("Informe a nota 2: ");
+        Double nota2 = input.nextDouble();
+
+        System.out.println("Informe a nota 3: ");
+        Double nota3 = input.nextDouble();
+
+        System.out.println("Informe o numero de faltas: ");
+        Double faltas = input.nextDouble();
+
+        Double mediaNota = (nota1 + nota2 + nota3) / 3.0;
+        Double minimoFaltas = 68 * 0.25;
+
+        if(mediaNota < 7.0){
+            if(faltas > minimoFaltas){
+                System.out.printf("Reprovado por nota e por faltas. Sua nota é %.1f", mediaNota);
+            }else{
+                System.out.printf("Reprovado por nota. Sua nota é %.1f", mediaNota);
+            }
+        }else if(faltas > minimoFaltas){
+            System.out.printf("Reprovado por faltas, suas faltas ultrapassaram o minimo aceitavel");
+        }else{
+            System.out.printf("Aprovado, sua média é %.1f", mediaNota);
+        }
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        CalculaMedia calculaMedia = new CalculaMedia();
+        calculaMedia.media();
+    }
+}
